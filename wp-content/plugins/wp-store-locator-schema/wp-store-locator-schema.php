a66e70bc04585773e3740310bbb955a07d1d0cda
slopeside-graham <graham@slopesidetechnology.com>
2021-11-10 16:50:45 -0700

    Initial Plugin Creation

---
+++ wp-content/plugins/wp-store-locator-schema/wp-store-locator-schema.php
@@ -0,0 +1,7 @@
+<?php
+/*
+Plugin Name: WP Store Locator
+Description: An easy to use location management system that enables users to search for nearby physical stores
+Author: Tijmen Smit
+Author URI: https://wpstorelocator.co/
+Version: 2.2.234
